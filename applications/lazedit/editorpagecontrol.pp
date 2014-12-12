3f2f88e69a904ffc4777dfdfc7ca81d23985c6fb
lazarus-bart <lazarus-bart@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2014-12-12 23:17:35 +0000

    LazEdit: update links to the authors website.
    git-svn-id: https://svn.code.sf.net/p/lazarus-ccr/svn@3838 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- applications/lazedit/editorpagecontrol.pp
+++ applications/lazedit/editorpagecontrol.pp
@@ -1,7 +1,7 @@
 { EditorPageControl unit
 
   Copyright (C) 2012 by Bart Broersma & Flying Sheep Inc.
-  http://home.tiscali.nl/~knmg0017/
+  http://www.flyingsheep.nl/
 
   This library is free software; you can redistribute it and/or modify it
   under the terms of the GNU Library General Public License as published by
