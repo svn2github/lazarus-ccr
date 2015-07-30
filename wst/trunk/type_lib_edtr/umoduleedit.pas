f666da77a0569af84a99708e993cfe7a687b3ea5
inoussa <inoussa@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2015-07-30 19:16:53 +0000

    defaultElementForm, defaultAttributeForm : Type Library Editor Support.
    git-svn-id: https://svn.code.sf.net/p/lazarus-ccr/svn@4232 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- wst/trunk/type_lib_edtr/umoduleedit.pas
+++ wst/trunk/type_lib_edtr/umoduleedit.pas
@@ -19,7 +19,7 @@ interface
 uses
   Classes, SysUtils, LResources, Forms, Controls, Graphics, Dialogs, ExtCtrls,
   ComCtrls, StdCtrls, ActnList, Buttons,
-  pastree, pascal_parser_intf,
+  pastree, pascal_parser_intf, xsd_consts,
   edit_helper;
 
 type
@@ -37,6 +37,8 @@ type
     Label2: TLabel;
     PageControl1: TPageControl;
     Panel1: TPanel;
+    edtDefaultElementForm: TRadioGroup;
+    edtDefaultAttributeForm: TRadioGroup;
     TabSheet1: TTabSheet;
     procedure actOKExecute(Sender: TObject);
     procedure actOKUpdate(Sender: TObject);
@@ -73,16 +75,42 @@ begin
   TAction(Sender).Enabled := not ( IsStrEmpty(edtName.Text) or IsStrEmpty(edtNamespace.Text));
 end;
 
+function FormStringToIndex(const AStr : string) : Integer;
+begin
+  if (AStr = s_qualified) then
+    Result := 1
+  else if (AStr = s_unqualified) then
+    Result := 2
+  else
+    Result := 0;
+end;
+
 procedure TfModuleEdit.LoadFromObject();
+var
+  s : string;
 begin
   edtName.Text := FSymbolTable.GetExternalName(FObject);
   edtNamespace.Text := FSymbolTable.GetExternalName(FObject);
+  s := FSymbolTable.Properties.GetValue(FObject,s_elementFormDefault);
+  edtDefaultElementForm.ItemIndex := FormStringToIndex(s);
+  s := FSymbolTable.Properties.GetValue(FObject,s_attributeFormDefault);
+  edtDefaultAttributeForm.ItemIndex := FormStringToIndex(s);
 end;
 
 procedure TfModuleEdit.SaveToObject();
+var
+  s : string;
 begin
   FObject.Name := ExtractIdentifier(edtName.Text);
   FSymbolTable.RegisterExternalAlias(FObject,Trim(edtNamespace.Text));
+  s := '';
+  if (edtDefaultElementForm.ItemIndex > 0) then
+    s := edtDefaultElementForm.Items[edtDefaultElementForm.ItemIndex];
+  FSymbolTable.Properties.SetValue(FObject,s_elementFormDefault,s);
+  s := '';
+  if (edtDefaultAttributeForm.ItemIndex > 0) then
+    s := edtDefaultAttributeForm.Items[edtDefaultAttributeForm.ItemIndex];
+  FSymbolTable.Properties.SetValue(FObject,s_attributeFormDefault,s);
 end;
 
 function TfModuleEdit.UpdateObject(
