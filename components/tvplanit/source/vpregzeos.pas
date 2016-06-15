c836531e3842158741e2768fab49620b4b1502dd
wp_xxyyzz <wp_xxyyzz@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2016-06-15 16:13:17 +0000

    tvplanit: Add icon for Zeos datastore. Some cleanup.
    git-svn-id: https://svn.code.sf.net/p/lazarus-ccr/svn@4758 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- components/tvplanit/source/vpregzeos.pas
+++ components/tvplanit/source/vpregzeos.pas
@@ -1,40 +1,17 @@
 unit VpRegZEOS;
   {-Registration unit for the ZEOS database components}
 
-{$I Vp.INC}    { Compiler Version Defines }
-//{$R VpReg.RES} { Palette Glyphs           }
+{$I Vp.INC}        // Compiler version defines
+{$R vpregzeos.res} // Palette glyphs
 
 interface
 
 uses
-  Windows, Dialogs,
- {$IFDEF FPC}
-  PropEdits,
- {$ELSE}
+ {$IFDEF DELPHI}
+ // Windows,
   {$IFDEF VERSION6} DesignIntf, DesignEditors, {$ELSE} DsgnIntf, {$ENDIF}
  {$ENDIF}
-  ZPropertyEditor,
-  Classes, Controls, TypInfo;
-                                    (*
-type
-  { Implements a property editor for VpZEOSDatastore.Database property. }
-  TVPZeosDatabasePropertyEditor = class(TZDatabasePropertyEditor)
-  public
-    function GetZComponent: TPersistent; override;
-  end;
-
-  { Implements a property editor for VpZEOSDatastore.LibLocation property. }
-  TVPZeosLibLocationPropertyEditor = class(TZLibLocationPropertyEditor)
-  public
-    function GetZComponent: TPersistent; override;
-  end;
-
-  {** Implements a property editor for ZConnection.ClientCodePage property. }
-  TVpZeosClientCodepagePropertyEditor = class (TZClientCodePagePropertyEditor)
-  public
-    function GetZComponent: TPersistent; override;
-  end;
-                                      *)
+  Classes;
 
 procedure Register;
 
@@ -46,38 +23,6 @@ uses
 procedure Register;
 begin
   RegisterComponents('Visual PlanIt', [TVpZeosDatastore]);
-                               (*
-  RegisterPropertyEditor(TypeInfo(string), TVpZEOSDatastore, 'Protocol', TZProtocolPropertyEditor);
-  RegisterPropertyEditor(TypeInfo(string), TVpZEOSDatastore, 'Database', TVpZeosDatabasePropertyEditor);
-  RegisterPropertyEditor(TypeInfo(string), TVpZEOSDatastore, 'LibraryLocation', TVpZeosLibLocationPropertyEditor);
-  RegisterPropertyEditor(TypeInfo(string), TVpZEOSDatastore, 'ClientCodepage', TVpZeosClientCodePagePropertyEditor);
-  *)
 end;
-
-    (*
-{ TVpZeosDatabasePropertyEditor }
-
-function TVpZeosDatabasePropertyEditor.GetZComponent: TPersistent;
-begin
-  if (GetComponent(0) is TVpZeosDatastore) then
-    Result := (GetComponent(0) as TVpZeosDatastore).Connection;
-end;
-
-{ TVpZeosLibLocationPropertyEditor }
-
-function TVpZeosLibLocationPropertyEditor.GetZComponent: TPersistent;
-begin
-  if (GetComponent(0) is TVpZeosDatastore) then
-    Result := (GetComponent(0) as TVpZeosDatastore).Connection;
-end;
-
-{ TVpZeosClientCodePagePropertyEditor }
-
-function TVpZeosClientCodePagePropertyEditor.GetZComponent: TPersistent;
-begin
-  if (GetComponent(0) is TVpZeosDatastore) then
-    Result := (GetComponent(0) as TVpZeosDatastore).Connection;
-end;
-      *)
 
 end.
