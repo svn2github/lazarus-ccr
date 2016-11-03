83a96c4a1b4e14bcce5265547513445026b38657
gbamber <gbamber@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2016-11-03 17:19:37 +0000

    V 1.0.4 Licensing now ModifiedGPL
    git-svn-id: https://svn.code.sf.net/p/lazarus-ccr/svn@5310 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- components/poweredby/upoweredby.pas
+++ components/poweredby/upoweredby.pas
@@ -5,7 +5,18 @@
   This library is free software; you can redistribute it and/or modify it
   under the terms of the GNU Library General Public License as published by
   the Free Software Foundation; either version 2 of the License, or (at your
-  option) any later version.
+  option) any later version with the following modification:
+
+  As a special exception, the copyright holders of this library give you
+  permission to link this library with independent modules to produce an
+  executable, regardless of the license terms of these independent modules,and
+  to copy and distribute the resulting executable under terms of your choice,
+  provided that you also meet, for each linked independent module, the terms
+  and conditions of the license of that module. An independent module is a
+  module which is not derived from or based on this library. If you modify
+  this library, you may extend this exception to your version of the library,
+  but you are not obligated to do so. If you do not wish to do so, delete this
+  exception statement from your version.
 
   This program is distributed in the hope that it will be useful, but WITHOUT
   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -27,7 +38,7 @@ uses
   ExtCtrls, InterfaceBase, LCLType,LCLVersion,AboutPoweredbyunit;
 
 const
-  C_VERSIONSTRING = '1.0.3.0';
+  C_VERSIONSTRING = '1.0.4.0';
   C_WIDGETSET_GTK = 'GTK widget set';
   C_WIDGETSET_GTK2 = 'GTK 2 widget set';
   C_WIDGETSET_GTK3 = 'GTK 3 widget set';
@@ -117,7 +128,7 @@ begin
   AboutBoxAuthorname:='Gordon Bamber';
   AboutBoxOrganisation:='Public Domain';
   AboutBoxAuthorEmail:='minesadorada@charcodelvalle.com';
-  AboutBoxLicenseType:='LGPL';
+  AboutBoxLicenseType:='MODIFIEDGPL';
 end;
 Function TPoweredby.GetInfoLCLVersion:String;
 begin
