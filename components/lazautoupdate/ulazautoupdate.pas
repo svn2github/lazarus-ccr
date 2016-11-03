9ad4cd2fd41e4e50513d7dd7e4779952a3c8625d
gbamber <gbamber@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2016-11-03 17:17:51 +0000

    V 0.1.27 Licensing now ModifiedGPL
    git-svn-id: https://svn.code.sf.net/p/lazarus-ccr/svn@5307 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- components/lazautoupdate/ulazautoupdate.pas
+++ components/lazautoupdate/ulazautoupdate.pas
@@ -9,7 +9,18 @@ unit ulazautoupdate;
   This library is free software; you can redistribute it and/or modify it
   under the terms of the GNU Library General Public License as published by
   the Free Software Foundation; either version 2 of the License, or (at your
-  option) any later version.
+  option) any later version with the following modification:
+
+  As a special exception, the copyright holders of this library give you
+  permission to link this library with independent modules to produce an
+  executable, regardless of the license terms of these independent modules,and
+  to copy and distribute the resulting executable under terms of your choice,
+  provided that you also meet, for each linked independent module, the terms
+  and conditions of the license of that module. An independent module is a
+  module which is not derived from or based on this library. If you modify
+  this library, you may extend this exception to your version of the library,
+  but you are not obligated to do so. If you do not wish to do so, delete this
+  exception statement from your version.
 
   This program is distributed in the hope that it will be useful, but WITHOUT
   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -34,7 +45,7 @@ const
   C_OnlineAppPath =
     'http://sourceforge.net/projects/%s/files/%s/%s/download';
   // [updatepath,projectname,filename]
-  C_TLazAutoUpdateComponentVersion = '0.1.22';
+  C_TLazAutoUpdateComponentVersion = '0.1.27';
   C_LAUTRayINI = 'lauimport.ini';
 
 {
@@ -508,7 +519,7 @@ begin
   AboutBoxAuthorname := 'Gordon Bamber';
   //AboutBoxOrganisation (string)
   AboutBoxAuthorEmail := 'minesadorada@gmail.com';
-  AboutBoxLicenseType := 'LGPL';
+  AboutBoxLicenseType := 'MODIFIEDGPL';
 end;
 
 function TLazAutoUpdate.AppIsActive(const ExeName: string): boolean;
