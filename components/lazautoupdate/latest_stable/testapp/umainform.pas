48ec6d54fed7b3b9a1c6a18025ccdba6ad4e90b2
gbamber <gbamber@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2017-01-16 12:51:19 +0000

    Added loacal logfile logging
    git-svn-id: https://svn.code.sf.net/p/lazarus-ccr/svn@5663 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- components/lazautoupdate/latest_stable/testapp/umainform.pas
+++ components/lazautoupdate/latest_stable/testapp/umainform.pas
@@ -6,26 +6,31 @@ interface
 
 uses
   Classes, SysUtils, Forms, ComCtrls,
-  Buttons, StdCtrls,ulazautoupdate;
+  Buttons, StdCtrls,LazFileUtils,FileUtil,
+  ulazautoupdate,eventlog;
 CONST
 {$IFDEF WINDOWS}
    {$IFDEF CPU32}
       C_VERSIONSINNAME = 'testappwin32.ini';
       C_ZIPFILENAME = 'testappwin32.zip';
+      C_LogFileName = 'testappwin32log.txt';
    {$ENDIF}
    {$IFDEF CPU64}
       C_VERSIONSINNAME = 'testappwin64.ini';
       C_ZIPFILENAME = 'testappwin64.zip';
+      C_LogFileName = 'testappwin64log.txt';
    {$ENDIF}
 {$ENDIF}
   {$IFDEF LINUX}
    {$IFDEF CPU32}
       C_VERSIONSINNAME = 'testapplinux32.ini';
       C_ZIPFILENAME = 'testapplinux32.zip';
+      C_LogFileName = 'testapplinux32log.txt';
    {$ENDIF}
    {$IFDEF CPU64}
       C_VERSIONSINNAME = 'testapplinux64.ini';
       C_ZIPFILENAME = 'testapplinux64.zip';
+      C_LogFileName = 'testapplinux64log.txt';
    {$ENDIF}
 {$ENDIF}
 
@@ -50,6 +55,7 @@ type
     procedure cmd_updateToNewVersionClick(Sender: TObject);
     procedure FormActivate(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
     procedure LazAutoUpdate1DebugEvent(Sender: TObject; lauMethodName,
       lauMessage: string);
     procedure LazAutoUpdate1Downloaded(Sender: TObject; ResultCode,
@@ -57,7 +63,8 @@ type
     procedure LazAutoUpdate1NewVersionAvailable(Sender: TObject;
       Newer: boolean; OnlineVersion: string);
   private
-
+     Logger: TEventLog;
+     procedure WriteAndLog(szText: string);
   public
 
   end;
@@ -70,6 +77,10 @@ implementation
 {$R *.lfm}
 
 { Tmainform }
+procedure Tmainform.WriteAndLog(szText: string);
+begin
+  Logger.Info(szText);
+end;
 
 procedure Tmainform.FormCreate(Sender: TObject);
 begin
@@ -78,6 +89,19 @@ begin
   LazAutoUpdate1.ZipfileName:=C_ZIPFILENAME;
   lbl_Version.Caption:='Version: ' + LazAutoUpdate1.AppVersion;
   Caption:=Application.Title;
+  if FileExistsUTF8(C_LogFileName) then
+    DeleteFile(C_LogFileName);
+  Logger := TEventLog.Create(nil);
+  Logger.LogType := ltFile;
+  Logger.FileName := C_LogFileName;
+  Logger.Active := True;
+  Logger.Info('Start of Log');
+end;
+
+procedure Tmainform.FormDestroy(Sender: TObject);
+begin
+  Logger.Info('End of Log');
+  FreeAndNil(Logger);
 end;
 
 procedure Tmainform.cmd_NewVersionAvailableClick(Sender: TObject);
@@ -114,6 +138,7 @@ procedure Tmainform.LazAutoUpdate1DebugEvent(Sender: TObject; lauMethodName,
   lauMessage: string);
 begin
    StatusBar1.SimpleText:='Debug Message: (' + lauMethodName + ') ' + lauMessage;
+   WriteAndLog(StatusBar1.SimpleText);
 end;
 
 procedure Tmainform.LazAutoUpdate1Downloaded(Sender: TObject; ResultCode,
@@ -121,6 +146,7 @@ procedure Tmainform.LazAutoUpdate1Downloaded(Sender: TObject; ResultCode,
 begin
   StatusBar1.SimpleText:=Format('Downloaded. StatusCode=%d BytesDownloaded=%d',
   [ResultCode,BytesDownloaded]);
+  WriteAndLog(StatusBar1.SimpleText);
 end;
 
 procedure Tmainform.LazAutoUpdate1NewVersionAvailable(Sender: TObject;
@@ -129,7 +155,9 @@ begin
   If Newer then
    StatusBar1.SimpleText:='New version available. Online Version is ' + OnlineVersion
   else
-    StatusBar1.SimpleText:='Online version is not newer. Online Version is ' + OnlineVersion
+    StatusBar1.SimpleText:='Online version is not newer. Online Version is ' + OnlineVersion;
+
+  WriteAndLog(StatusBar1.SimpleText);
 
 end;
 
