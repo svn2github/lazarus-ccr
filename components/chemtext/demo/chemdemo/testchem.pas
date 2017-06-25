45c14ad51eae36587b01518569928b991359c3e0
wp_xxyyzz <wp_xxyyzz@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2017-06-25 15:56:27 +0000

    chemtext: Initial commit
    git-svn-id: https://svn.code.sf.net/p/lazarus-ccr/svn@5958 8e941d3f-bd1b-0410-a28a-d453659cc2b4

---
+++ components/chemtext/demo/chemdemo/testchem.pas
@@ -0,0 +1,44 @@
+unit testchem;
+
+interface
+
+uses
+  SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
+  StdCtrls, ExtCtrls, chemtext;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Spacer: TBevel;
+    ChemLabel1: TChemLabel;
+    ChemLabel2: TChemLabel;
+    ChemLabel3: TChemLabel;
+    ChemLabel5: TChemLabel;
+    Edit1: TEdit;
+    ChemLabel4: TChemLabel;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Panel1: TPanel;
+    procedure Edit1Change(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+procedure TForm1.Edit1Change(Sender: TObject);
+begin
+  ChemLabel4.Caption := Edit1.Text;
+end;
+
+end.
