0ae5d13c33d8380e73eb77174d1e3270720ad7b7
wp_xxyyzz <wp_xxyyzz@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2018-02-04 17:30:54 +0000

    spktoolbar: Update demo project forms because of removed property GroupBehavior in TSpkCheckbox and TSpkRadioButton. Add more panes to Styles demo.
    git-svn-id: https://svn.code.sf.net/p/lazarus-ccr/svn@6171 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- components/spktoolbar/demos/styles/unit1.pas
+++ components/spktoolbar/demos/styles/unit1.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
-  spktoolbar, spkt_Tab, spkt_Pane, spkt_Buttons;
+  spktoolbar, spkt_Tab, spkt_Pane, spkt_Buttons, spkt_Checkboxes;
 
 type
 
@@ -18,7 +18,13 @@ type
     Panel2: TPanel;
     Panel3: TPanel;
     Panel4: TPanel;
+    ScrollBox1: TScrollBox;
     SmallImages: TImageList;
+    SpkCheckbox1: TSpkCheckbox;
+    SpkCheckbox2: TSpkCheckbox;
+    SpkCheckbox3: TSpkCheckbox;
+    SpkCheckbox4: TSpkCheckbox;
+    SpkCheckbox5: TSpkCheckbox;
     SpkLargeButton1: TSpkLargeButton;
     SpkLargeButton10: TSpkLargeButton;
     SpkLargeButton11: TSpkLargeButton;
@@ -36,7 +42,17 @@ type
     SpkLargeButton9: TSpkLargeButton;
     SpkPane1: TSpkPane;
     SpkPane10: TSpkPane;
+    SpkPane11: TSpkPane;
+    SpkPane12: TSpkPane;
+    SpkPane13: TSpkPane;
+    SpkPane14: TSpkPane;
+    SpkPane15: TSpkPane;
+    SpkPane16: TSpkPane;
+    SpkPane17: TSpkPane;
+    SpkPane18: TSpkPane;
+    SpkPane19: TSpkPane;
     SpkPane2: TSpkPane;
+    SpkPane20: TSpkPane;
     SpkPane3: TSpkPane;
     SpkPane4: TSpkPane;
     SpkPane5: TSpkPane;
@@ -73,10 +89,50 @@ type
     SpkSmallButton33: TSpkSmallButton;
     SpkSmallButton34: TSpkSmallButton;
     SpkSmallButton35: TSpkSmallButton;
+    SpkSmallButton36: TSpkSmallButton;
+    SpkSmallButton37: TSpkSmallButton;
+    SpkSmallButton38: TSpkSmallButton;
+    SpkSmallButton39: TSpkSmallButton;
     SpkSmallButton4: TSpkSmallButton;
+    SpkSmallButton40: TSpkSmallButton;
+    SpkSmallButton41: TSpkSmallButton;
+    SpkSmallButton42: TSpkSmallButton;
+    SpkSmallButton43: TSpkSmallButton;
+    SpkSmallButton44: TSpkSmallButton;
+    SpkSmallButton45: TSpkSmallButton;
+    SpkSmallButton46: TSpkSmallButton;
+    SpkSmallButton47: TSpkSmallButton;
+    SpkSmallButton48: TSpkSmallButton;
+    SpkSmallButton49: TSpkSmallButton;
     SpkSmallButton5: TSpkSmallButton;
+    SpkSmallButton50: TSpkSmallButton;
+    SpkSmallButton51: TSpkSmallButton;
+    SpkSmallButton52: TSpkSmallButton;
+    SpkSmallButton53: TSpkSmallButton;
+    SpkSmallButton54: TSpkSmallButton;
+    SpkSmallButton55: TSpkSmallButton;
+    SpkSmallButton56: TSpkSmallButton;
+    SpkSmallButton57: TSpkSmallButton;
+    SpkSmallButton58: TSpkSmallButton;
+    SpkSmallButton59: TSpkSmallButton;
     SpkSmallButton6: TSpkSmallButton;
+    SpkSmallButton60: TSpkSmallButton;
+    SpkSmallButton61: TSpkSmallButton;
+    SpkSmallButton62: TSpkSmallButton;
+    SpkSmallButton63: TSpkSmallButton;
+    SpkSmallButton64: TSpkSmallButton;
+    SpkSmallButton65: TSpkSmallButton;
+    SpkSmallButton66: TSpkSmallButton;
+    SpkSmallButton67: TSpkSmallButton;
+    SpkSmallButton68: TSpkSmallButton;
+    SpkSmallButton69: TSpkSmallButton;
     SpkSmallButton7: TSpkSmallButton;
+    SpkSmallButton70: TSpkSmallButton;
+    SpkSmallButton71: TSpkSmallButton;
+    SpkSmallButton72: TSpkSmallButton;
+    SpkSmallButton73: TSpkSmallButton;
+    SpkSmallButton74: TSpkSmallButton;
+    SpkSmallButton75: TSpkSmallButton;
     SpkSmallButton8: TSpkSmallButton;
     SpkSmallButton9: TSpkSmallButton;
     SpkTab1: TSpkTab;
