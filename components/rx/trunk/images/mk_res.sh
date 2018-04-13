f19bec63700a22a0dcb8ed69f4c6aef31fd2243d
alexs75 <alexs75@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2018-04-13 08:58:45 +0000

    RxFPC:new component images from zoltanleo aka Док
    git-svn-id: https://svn.code.sf.net/p/lazarus-ccr/svn@6303 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- components/rx/trunk/images/mk_res.sh
+++ components/rx/trunk/images/mk_res.sh
@@ -1,3 +1,25 @@
 rm rx.lrs
 rm rx.res
-/usr/local/share/lazarus/tools/lazres rx.res TRxDBDateEdit.png TRXLookUpEdit.xpm TRxDBCalcEdit.xpm TRxDBLookupCombo.xpm TRxDBGrid.xpm TDualListDialog.xpm TFolderLister.xpm TRxMemoryData.xpm TCURRENCYEDIT.xpm TRXSWITCH.xpm TRXDICE.xpm TRXDBCOMBOBOX.xpm ttoolpanel.xpm TPAGEMANAGER.xpm TRXAPPICON.xpm TSECRETPANEL.xpm TRXLABEL.xpm tautopanel.xpm TRxCalendarGrid.xpm TRxClock.png TRxSpeedButton.png TRxSpinButton.png TRxSpinEdit.png TRXDBSpinEdit.png TRxTimeEdit.png TRxDBTimeEdit.png TRxDBProgressBar.png TRxDBTrackBar.png TRxLoginDialog.png TRxVersionInfo.png TRxAboutDialog.png TRxDBCurrEdit.png TRxViewsPanel.png TRxDBColorBox.png TRxRadioGroup.png TRxDBRadioGroup.png TRxHistoryNavigator.png TRxCloseFormValidator.png TRxIniPropStorage.png TRxDBGridFooterTools.png TRxDateRangeEdit.png TRxMDICloseButton.png TRxMDIPanel.png TRxMDITasks.png TRxDBGridExportPDF.png TRxDBVerticalGrid.png TRxRangeSelector.png TRxDateEdit_150.png  TRxDateEdit_200.png  TRxDateEdit.png
+/usr/local/share/lazarus/tools/lazres rx.res TRxDateEdit_150.png  TRxDateEdit_200.png  TRxDateEdit.png \
+  TRxLookupEdit_150.png  TRxLookupEdit_200.png  TRxLookupEdit.png \
+  TRxDBGrid.png TRxDBGrid_150.png TRxDBGrid_200.png \
+  TRxDBGridExportPDF.png TRxDBGridExportPDF_150.png TRxDBGridExportPDF_200.png \
+  TRxDBGridFooterTools.png TRxDBGridFooterTools_150.png TRxDBGridFooterTools_200.png \
+  TRxDBCalcEdit.png TRxDBCalcEdit_150.png TRxDBCalcEdit_200.png \
+  TRxDBColorBox.png TRxDBColorBox_150.png TRxDBColorBox_200.png \
+  TRxDBCurrEdit.png TRxDBCurrEdit_150.png TRxDBCurrEdit_200.png \
+  TRxMemoryData.png TRxMemoryData_150.png TRxMemoryData_200.png \
+  TRxDBVerticalGrid.png TRxDBVerticalGrid_150.svg TRxDBVerticalGrid_200.png \
+  TRxDBTrackBar.png TRxDBTrackBar_150.png TRxDBTrackBar_200.png \
+  TRxDBTimeEdit.png TRxDBTimeEdit_150.png TRxDBTimeEdit_200.png \
+  TRxDBSpinEdit.png TRxDBSpinEdit_150.png TRxDBSpinEdit_200.png \
+  TRxDBDateEdit.png TRxDBDateEdit_150.png TRxDBDateEdit_200.png \
+  TRxDBComboBox.png TRxDBComboBox_150.png TRxDBComboBox_200.png \
+  TRxDBRadioGroup.png TRxDBRadioGroup_150.png TRxDBRadioGroup_200.png \
+  TRxDBProgressBar.png TRxDBProgressBar_150.png TRxDBProgressBar_200.png \
+  TRxDBLookupCombo.png TRxDBLookupCombo_150.png TRxDBLookupCombo_200.png \
+  TDualListDialog.xpm TFolderLister.xpm TCURRENCYEDIT.xpm TRXSWITCH.xpm TRXDICE.xpm ttoolpanel.xpm TPAGEMANAGER.xpm TRXAPPICON.xpm TSECRETPANEL.xpm TRXLABEL.xpm tautopanel.xpm TRxCalendarGrid.xpm TRxClock.png TRxSpeedButton.png TRxSpinButton.png TRxSpinEdit.png \
+  TRxTimeEdit.png \
+  TRxLoginDialog.png TRxVersionInfo.png TRxAboutDialog.png \
+  TRxViewsPanel.png \
+  TRxHistoryNavigator.png TRxCloseFormValidator.png TRxIniPropStorage.png TRxDateRangeEdit.png TRxMDICloseButton.png TRxMDIPanel.png TRxMDITasks.png TRxRangeSelector.png 
