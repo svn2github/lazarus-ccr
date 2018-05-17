4d0398dca4735aead4c41b9bfaf880db477ef209
alexs75 <alexs75@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2018-05-17 11:29:02 +0000

    RxFPC:new components icon from zoltanleo
    git-svn-id: https://svn.code.sf.net/p/lazarus-ccr/svn@6427 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- components/rx/trunk/images/mk_res.sh
+++ components/rx/trunk/images/mk_res.sh
@@ -1,7 +1,7 @@
 rm rx.lrs
 rm rx.res
-/usr/local/share/lazarus/tools/lazres rx.res TRxDateEdit_150.png  TRxDateEdit_200.png  TRxDateEdit.png \
-  TRxLookupEdit_150.png  TRxLookupEdit_200.png  TRxLookupEdit.png \
+/usr/local/share/lazarus/tools/lazres rx.res TRxDateEdit_150.png  TRxDateEdit_200.png TRxDateEdit.png \
+  TRxLookupEdit.png TRxLookupEdit_150.png TRxLookupEdit_200.png \
   TRxDBGrid.png TRxDBGrid_150.png TRxDBGrid_200.png \
   TRxDBGridExportPDF.png TRxDBGridExportPDF_150.png TRxDBGridExportPDF_200.png \
   TRxDBGridFooterTools.png TRxDBGridFooterTools_150.png TRxDBGridFooterTools_200.png \
@@ -18,8 +18,29 @@ rm rx.res
   TRxDBRadioGroup.png TRxDBRadioGroup_150.png TRxDBRadioGroup_200.png \
   TRxDBProgressBar.png TRxDBProgressBar_150.png TRxDBProgressBar_200.png \
   TRxDBLookupCombo.png TRxDBLookupCombo_150.png TRxDBLookupCombo_200.png \
-  TDualListDialog.xpm TFolderLister.xpm TCURRENCYEDIT.xpm TRXSWITCH.xpm TRXDICE.xpm ttoolpanel.xpm TPAGEMANAGER.xpm TRXAPPICON.xpm TSECRETPANEL.xpm TRXLABEL.xpm tautopanel.xpm TRxCalendarGrid.xpm TRxClock.png TRxSpeedButton.png TRxSpinButton.png TRxSpinEdit.png \
-  TRxTimeEdit.png \
-  TRxLoginDialog.png TRxVersionInfo.png TRxAboutDialog.png \
-  TRxViewsPanel.png \
-  TRxHistoryNavigator.png TRxCloseFormValidator.png TRxIniPropStorage.png TRxDateRangeEdit.png TRxMDICloseButton.png TRxMDIPanel.png TRxMDITasks.png TRxRangeSelector.png 
+  TDualListDialog.png TDualListDialog_150.png TDualListDialog_200.png \
+  TFolderLister.png TFolderLister_150.png TFolderLister_200.png \
+  TCurrencyEdit.png TCurrencyEdit_150.png TCurrencyEdit_200.png \
+  TRxSwitch.png TRxSwitch_150.png TRxSwitch_200.png \
+  TRxDice.png TRxDice_150.png TRxDice_200.png \
+  TToolPanel.png TToolPanel_150.png TToolPanel_200.png \
+  TPageManager.png TPageManager_150.png TPageManager_200.png \
+  TSecretPanel.png TSecretPanel_150.png TSecretPanel_200.png \
+  TRxLabel.png TRxLabel_150.png TRxLabel_200.png \
+  TAutoPanel.png TAutoPanel_150.png TAutoPanel_200.png \
+  TRxCalendarGrid.png TRxCalendarGrid_150.png TRxCalendarGrid_200.png \
+  TRxClock.png TRxClock_150.png TRxClock_200.png  \
+  TRxSpeedButton.png TRxSpeedButton_150.png TRxSpeedButton_200.png \
+  TRxSpinButton.png TRxSpinButton_150.png TRxSpinButton_200.png \
+  TRxSpinEdit.png TRxSpinEdit_150.png TRxSpinEdit_200.png \
+  TRxTimeEdit.png TRxTimeEdit_150.png TRxTimeEdit_200.png \
+  TRxAboutDialog_150.png TRxAboutDialog_200.png TRxAboutDialog.png \
+  TRxViewsPanel.png TRxViewsPanel_150.png TRxViewsPanel_200.png \
+  TRxDateRangeEdit.png TRxDateRangeEdit_150.png TRxDateRangeEdit_200.png \
+  TRxMDICloseButton.png TRxMDICloseButton_150.png TRxMDICloseButton_200.png  \
+  TRxMDIPanel.png TRxMDIPanel_150.png TRxMDIPanel_200.png \
+  TRxMDITasks.png TRxMDITasks_150.png TRxMDITasks_200.png \
+  TRxRadioGroup.png TRxRadioGroup_150.png TRxRadioGroup_200.png \
+  TRxRangeSelector.png TRxRangeSelector_150.png TRxRangeSelector_200.png \
+  TRxHistoryNavigator.png TRxCloseFormValidator.png TRxIniPropStorage.png \
+  TRxLoginDialog.png TRxVersionInfo.png
