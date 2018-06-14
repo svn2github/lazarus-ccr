63ec29e50d979384dc0d7570807d24fd23837d5a
alexs75 <alexs75@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2018-06-14 07:21:36 +0000

    RxFPC:new version component images from zoltanleo
    git-svn-id: https://svn.code.sf.net/p/lazarus-ccr/svn@6496 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- components/rx/trunk/images/componet_img/all_component_res.sh
+++ components/rx/trunk/images/componet_img/all_component_res.sh
@@ -9,5 +9,4 @@ rm pickdate.rex
 /usr/local/share/lazarus/tools/lazres rx_lcl.res picDateEdit.png rxbtn_downarrow.png rx_range_h_back.png rx_range_h_sel.png rx_slader_bottom.png rx_slader_top.png rx_range_v_back.png rx_range_v_sel.png rx_slader_left.png rx_slader_right.png
 
 /usr/local/share/lazarus/tools/lazres pickdate.res rx_next1.png rx_next2.png rx_prev1.png rx_prev2.png
-/usr/local/share/lazarus/tools/lazres rxswitch.res rxswitch_off.png  rxswitch_on.png
-
+/usr/local/share/lazarus/tools/lazres rxswitch.res rxswitch_off.png  rxswitch_on.png rx_ButtonOffHor.png rx_ButtonOffVertDown.png rx_ButtonOffVertUp.png rx_ButtonOnHor.png rx_ButtonOnVertDown.png rx_ButtonOnVertUp.png
