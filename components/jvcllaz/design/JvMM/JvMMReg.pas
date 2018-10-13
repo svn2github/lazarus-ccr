cf5d3d6f4fe65c26563eca50cc4bc8ae0630c93b
wp_xxyyzz <wp_xxyyzz@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2018-10-13 21:13:58 +0000

    jvcllaz: Add full color controls and dialogs
    git-svn-id: https://svn.code.sf.net/p/lazarus-ccr/svn@6684 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- components/jvcllaz/design/JvMM/JvMMReg.pas
+++ components/jvcllaz/design/JvMM/JvMMReg.pas
@@ -18,6 +18,8 @@ uses
   JvDsgnConsts,
   JvId3v1, JvId3v2Base, JvId3v2, JvGradient, JvId3v2EditorForm,
   JvGradientHeaderPanel, JvSpecialProgress,
+  JvFullColorSpaces, JvFullColorCtrls, JvFullColorEditors, JvFullColorSpacesEditors,
+  JvFullColorForm, JvFullColorDialogs,
   JvBmpAnimator;
 
 procedure Register;
@@ -26,10 +28,25 @@ begin
     TJvId3v1, TJvId3v2,
     TJvBmpAnimator,
     TJvGradient, TJvGradientHeaderPanel,
-    TJvSpecialProgress
+    TJvSpecialProgress,
+    TJvFullColorPanel, TJvFullColorTrackBar, TJvFullColorGroup, TJvFullColorLabel,
+    TJvFullColorSpaceCombo, TJvFullColorAxisCombo, TJvFullColorCircle,
+    TJvFullColorDialog, TJvFullColorCircleDialog
   ]);
+
   RegisterComponentEditor(TJvID3Controller, TJvID3ControllerEditor);
   RegisterPropertyEditor(TypeInfo(TJvID3FileInfo), nil, '', TJvID3FileInfoEditor);
+
+  RegisterPropertyEditor(TypeInfo(TJvFullColorSpaceID), nil, '', TJvColorIDEditor);
+  RegisterPropertyEditor(TypeInfo(TJvFullColor), nil, '', TJvFullColorProperty);
+  {
+  RegisterPropertyEditor(TypeInfo(TJvFullColorList), nil, '', TJvFullColorListEditor);
+  RegisterSelectionEditor(TJvFullColorPanel, TJvFullColorSelection);
+  RegisterSelectionEditor(TJvFullColorCircle, TJvFullColorSelection);
+  RegisterSelectionEditor(TJvFullColorLabel, TJvFullColorSelection);
+  RegisterSelectionEditor(TJvFullColorSpaceCombo, TJvFullColorSelection);
+  RegisterSelectionEditor(TJvFullColorAxisCombo, TJvFullColorSelection);
+ }
 end;
 
 end.
